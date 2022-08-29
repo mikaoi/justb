8bf258a39deb128038fe0c5d14f383407f2dd034
Marnikilis <diana@cravter.com>
2022-08-29 18:56:56 +0300

    latest changes

--- src/styles/theme/theme.js
+++ src/styles/theme/theme.js
@@ -1,4 +1,5 @@
 import { createTheme } from "@mui/material/styles";
+import SchemeRegular from "../fonts/SchemeRg-Regular.woff2";
 
 const breakpoints = {
   values: {
@@ -45,7 +46,18 @@ export const theme = createTheme({
           minWidth: "100vw",
           color: "#31476E",
           fontSize: "20px",
+          fontFamily: "Scheme",
         },
+        styleOverrides: `
+        @font-face {
+          font-family: 'SchemeRg-Regular';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: local('SchemeRg-Regular'), local('SchemeRg-Regular'), url(${SchemeRegular}) format('woff2');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
       },
     },
     MuiLink: {
