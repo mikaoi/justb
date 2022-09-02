e0d572aecc1e9eed8d61c8e0f8390c1030f530b0
Marcus <marcusharrell597@icloud.com>
2022-09-02 12:28:21 -0400

    Remove red from tooltip text

--- src/components/HomePage/Tooltip.js
+++ src/components/HomePage/Tooltip.js
@@ -45,7 +45,7 @@ const Tooltip = () => {
     >
       <Typography variant={"subtitle2"}>
         {month + " " + today + ", " + year + ", Let’s get "}
-        <Box sx={{ color: "secondary.main" }} component={"span"}>
+        <Box sx={{ color: "main.dark" }} component={"span"}>
           moving!
         </Box>
       </Typography>
