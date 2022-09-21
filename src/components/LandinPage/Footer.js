f2de134bb62c7735425e780dd2503efa060ef7e6
Marcus Harrell <marcusharrell597@icloud.com>
2022-09-21 14:32:00 -0400

    Footer changes

--- src/components/LandinPage/Footer.js
+++ src/components/LandinPage/Footer.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import blueBg from "../../styles/images/langingImg/blueBg.svg";
 import page from "../../styles/images/langingImg/page.png";
+import footerCharacter from "../../styles/images/langingImg/footerCharacter.png"
+import {LandingButton} from "../../styles/theme/styledComponents";
+import Socials from "../../styles/images/langingImg/socials.svg"
 
 const Footer = () => {
   return (
-    <Box sx={{ height: "1342px", width: "100%", marginTop: "-110px" }}>
+    <Box sx={{ height: "1000px", width: "100%", marginTop: "-200px" }}>
       <Box
         sx={{
           padding: "100px 10px",
@@ -19,17 +22,21 @@ const Footer = () => {
           backgroundSize: "cover",
           color: "lightColor.light",
           gap: "100px",
-          marginTop:'200px'
+         
         }}
       >
         <Box
           sx={{
             display: "flex",
-            width: "100%",
+            width: "65%",
             justifyContent: "space-between",
+            marginTop:"250px"
           }}
         >
-          
+           <img src={footerCharacter} alt={"Characters"} width={"100%"}/>
+        </Box>
+        <Box>
+        <LandingButton sx={{backgroundColor: "primary.main", color: "#31476E", padding: '15px'}}>Get Moving</LandingButton>
         </Box>
       </Box>
     </Box>
