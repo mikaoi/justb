9b21c503fa8570c2a446dcb6d41f6696525d1f28
Marcus Harrell <marcusharrell597@icloud.com>
2022-09-21 23:36:46 -0400

    FAQ accordion and header

--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -1,8 +1,34 @@
 import React from "react";
 import MainLayout from "../components/MainLayout/MainLayout";
+import FaqAccordions from "../components/faq/FaqAccordions"
+import FaqHeader from "../components/faq/FaqHeader"
+import FaqFooter from "../components/faq/FaqFooter"
+import AccordionControlled from "../components/faq/AccordionControlled";
+
+// ** Other imports
+// ** React Imports
+import { Fragment, useEffect, useState } from 'react'
+
+// ** MUI Imports
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+
+// ** Icons Imports
+import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
+
+
+
 
 const HelpPage = () => {
-  return <MainLayout>Help Page</MainLayout>;
+  return (
+    <MainLayout>
+    <FaqHeader/>
+    <AccordionControlled/>
+    </MainLayout>
+
+
+ ) 
 };
 
 export default HelpPage;
