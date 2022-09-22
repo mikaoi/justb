0f6b31daa055fbcb2924e481ab431b2a2e85bb73
Marcus Harrell <marcusharrell597@icloud.com>
2022-09-22 12:17:32 -0400

    FAQ changes

--- src/components/faq/AccordionControlled.js
+++ src/components/faq/AccordionControlled.js
@@ -32,7 +32,7 @@ const AccordionControlled = () => {
           <Typography>What is justb?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
+        <Typography variant="subtitle2">
             justb creates inclusive movement videos for extraordinary teachers,
             kids, and classrooms. justb is designed through the lens of
             neurodivergent students to create movement breaks that support as
@@ -54,14 +54,12 @@ const AccordionControlled = () => {
           <Typography>What is required to use justb?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
+        <Typography variant="subtitle2">
             justb is designed for use in the average classroom or home. All the
-            user needs is: <br>
-            </br>- A computer with up to date browsers. <br>
-            </br>- A broadband
-            internet connection. <br>
-            </br>- A screen that all students can see (e.g., a
-            projector, interactive whiteboard, or big monitor).
+            user needs is: <br></br>- A computer with up to date browsers.{" "}
+            <br></br>- A broadband internet connection. <br></br>- A screen that
+            all students can see (e.g., a projector, interactive whiteboard, or
+            big monitor).
           </Typography>
         </AccordionDetails>
       </Accordion>
@@ -78,9 +76,54 @@ const AccordionControlled = () => {
           <Typography>When should I use justb?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Gingerbread lemon drops bear claw gummi bears bonbon wafer jujubes
-            tiramisu. Jelly pie cake. Sweet roll dessert sweet pastry powder.
+        <Typography variant="subtitle1">
+            justb is designed to be used at the discrepancy of our users. It’s
+            available to be used 24/7.
+            <Typography variant="subtitle2">
+              1. Use justb consistently - everyday, (even if it’s only 5
+              minutes) consistency is essential. <br></br>2. Use justb before
+              you NEED it. justb will help kids regulate their bodies and
+              emotions. Kids learn all new skills best when they are regulated,
+              justb helps kids grow their toolbox of regulation strategies.
+            </Typography>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel4"}
+        onChange={handleChange("panel4")}
+      >
+        <AccordionSummary
+          expandIcon={<ChevronDown />}
+          id="controlled-panel-header-4"
+          aria-controls="controlled-panel-content-4"
+        >
+          <Typography>How to navigate and find classes</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>*Screen shot or video explanation</Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel5"}
+        onChange={handleChange("panel5")}
+      >
+        <AccordionSummary
+          expandIcon={<ChevronDown />}
+          id="controlled-panel-header-5"
+          aria-controls="controlled-panel-content-5"
+        >
+          <Typography>Share Links to Videos</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        <Typography variant="subtitle2">
+            You can share links to specific videos with your students in your
+            virtual classrooms or through emails by using our share feature.
+            These share links will allow others to view specific GoNoodle videos
+            without the need to log in or sign up. Use this feature to make it
+            easy for parents to try out your favorite GoNoodle videos, and
+            encourage them to create free accounts to access the rest of the
+            GoNoodle library.
           </Typography>
         </AccordionDetails>
       </Accordion>
