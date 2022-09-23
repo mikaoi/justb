61f2130d6529fdeb97e35b21125e0bc33141260b
Marcus Harrell <marcusharrell597@icloud.com>
2022-09-23 15:34:47 -0400

    Change landing to login

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,85 +1,62 @@
 import React from "react";
 import bg from "../styles/images/langingImg/heroBg.png";
-import {Box, Typography} from "@mui/material";
-import appImg from "../styles/images/langingImg/app.png";
-import {LandingButton} from "../styles/theme/styledComponents";
-import MoveWithUsSection from "../components/LandingPage/MoveWithUsSection";
-import Testimonials from "../components/LandingPage/Testimonials";
-import Footer from "../components/LandingPage/Footer";
-import Header from "../components/LandingPage/Header";
-import IconsBox from "../components/LandingPage/IconsBox";
+import { AppBar, Box, Typography } from "@mui/material"; 
+import FormLayoutsBasic from "../views/forms/form-layouts/FormLayoutsBasic";
+import { CardBox } from "../styles/theme/styledComponents";
+import BlankLayout from "../components/MainLayout/BlankLayout";
+import { Toolbar}from "@mui/material";
+import { Button }from "@mui/material";
+import { IconButton } from "@mui/material";
+import { MenuItem }from "@mui/material";
+
 
 const LandingPage = () => {
   return (
-    <Box sx={{
-      color: "lightColor.main",
-      overflow: "hidden",
-      backgroundColor: "#F2F2F2"
-    }}>
-      <Header/>
-      <Box
-        sx={{
-          paddingTop: "130px",
-          paddingX: "120px",
-          minHeight: "calc(100vh + 100px)",
-          display: "flex",
-          position: "relative",
-          backgroundImage: `url(${bg})`,
-          backgroundSize: "cover",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            position: "relative",
-            alignItems: "center",
-            height: "100%"
-          }}
-        >
-          <Box sx={{marginLeft: "50px"}}>
-            <Typography
-              sx={{
-                fontWeight: 700,
-                color: "inherit",
-                fontSize: "40px",
-                marginBottom: "15px",
-              }}
-            >
-              Enriching Lives Through Movement
-            </Typography>
-            <Typography
-              sx={{
-                fontWeight: 100,
-                color: "inherit",
-                fontSize: "24px",
-                marginBottom: "50px",
-              }}
-            >
-              An inclusive and interactive movement platform for teachers, kids,
-              and classrooms
-            </Typography>
-            <LandingButton sx={{padding: "10px", backgroundColor: "primary.main"}}>
-              get moving
-            </LandingButton>
-          </Box>
-          <Box sx={{maxWidth: "800px"}}>
-            <img src={appImg} alt={"App"} width={"100%"}/>
-            <Typography sx={{
-              textAlign: "center",
-              marginTop: "-16px",
-              fontSize: "15px",
-              color: "inherit"
-            }}>
-              Designed by Olympic athletes OT’s, teachers, and kids!
-            </Typography>
-          </Box>
-        </Box>
-      </Box>
-      <MoveWithUsSection/>
-      <IconsBox/>
-      <Testimonials/>
-      <Footer/>
+    
+    <div style={{ backgroundImage: `url(${bg})`,  backgroundSize: "cover",
+    backgroundRepeat: "no-repeat", width: "100%", }}>
+    <BlankLayout>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuItem />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </Box>
+      <Box className='content-center'>
+    <CardBox
+      sx={{
+        paddingTop: { xs: "30px", "2xl": "40px" },
+        paddingBottom: "15px",
+        paddingX: { xs: "30px", "2xl": "40px", "3xl": "50px" },
+        display: "flex",
+        flexDirection: "column",
+        gap: { xs: "40px", "2xl": "20px" },
+        justifyContent: "space-between",
+        width: "50%",
+        height: "100%",
+        boxShadow: 2,
+      }}
+    >
+      <Box>
+        <FormLayoutsBasic />
+      </Box>
+    </CardBox>
+    </Box>
+    </BlankLayout>
+    </div>
   );
 };
 
